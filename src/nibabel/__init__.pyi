754cd3aef11e05e4b9d1fb510ada4a5d36f63aa2
Keji Li <kli@acadian-asset.com>
2025-04-28 11:47:53 -0400

    initial commit for nibabel

---
+++ src/nibabel/__init__.pyi
@@ -0,0 +1,14 @@
+from typing import Any, Literal
+import numpy as np
+import numpy.typing as npt
+from nibabel import Nifti1Image
+
+def load(filename: str) -> Nifti1Image: ...
+
+def save(img: Nifti1Image, filename: str): ...
+
+class Nifti1Image:
+    affine: np.ndarray[tuple[Literal[4], Literal[4]], np.dtype[np.float_]]
+
+    def get_fdata(self, caching: Literal['fill', 'unchanged'] = 'fill',
+                  dtype: npt.DTypeLike = np.float64) -> np.ndarray[Any, np.dtype[np.floating]]: ...
