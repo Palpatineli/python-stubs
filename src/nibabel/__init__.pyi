1eb4f9965777b024fba275c9795b5760f4003a9a
Keji Li <kli@acadian-asset.com>
2025-04-28 11:59:46 -0400

    fix: allow pathlike in place of str

--- src/nibabel/__init__.pyi
+++ src/nibabel/__init__.pyi
@@ -1,11 +1,12 @@
 from typing import Any, Literal
+import os
 import numpy as np
 import numpy.typing as npt
 from nibabel import Nifti1Image
 
-def load(filename: str) -> Nifti1Image: ...
+def load(filename: os.PathLike[str]) -> Nifti1Image: ...
 
-def save(img: Nifti1Image, filename: str): ...
+def save(img: Nifti1Image, filename: os.PathLike[str]): ...
 
 class Nifti1Image:
     affine: np.ndarray[tuple[Literal[4], Literal[4]], np.dtype[np.float_]]
